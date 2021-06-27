fb6fd344eede01b3a3d7a81045394f6da6ab95b0
Sumit Srivastava <sumit.srivastava2805@gmail.com>
2021-06-27 17:38:36 +0100

    feat: Find the Sum of Maximum Sum Subarray

---
+++ src/main/java/com/problemsolving/array/Solution15.java
@@ -0,0 +1,33 @@
+package com.problemsolving.array;
+
+import java.util.Arrays;
+
+class Solution15 {
+    public static int findMaxSumSubArray(int[] arr) {
+        if (arr.length < 1) {
+            return 0;
+        }
+
+        int currMax = arr[0];
+        int globalMax = arr[0];
+        int lengtharray = arr.length;
+        for (int i = 1; i < lengtharray; i++) {
+            if (currMax < 0) {
+                currMax = arr[i];
+            } else {
+                currMax += arr[i];
+            }
+
+            if (globalMax < currMax) {
+                globalMax = currMax;
+            }
+        }
+        return globalMax;
+    }
+
+    public static void main(String args[]) {
+        int[] arr1 = {1, 7, -2, -5, 10, -1};
+        System.out.println("Array: " + Arrays.toString(arr1));
+        System.out.println("Sum: " + findMaxSumSubArray(arr1));
+    }
+}
