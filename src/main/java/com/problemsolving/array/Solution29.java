ebd1c2124d6467c9b337d33d38928e8429922330
Sumit Srivastava <sumit.srivastava2805@gmail.com>
2021-06-29 23:57:44 +0100

    feat: Subsets

--- src/main/java/com/problemsolving/array/Solution29.java
+++ src/main/java/com/problemsolving/array/Solution29.java
@@ -2,25 +2,19 @@ package com.problemsolving.array;
 
 import java.util.*;
 
-class Solution29 {
+class Subsets {
 
     public static List<List<Integer>> findSubsets(int[] nums) {
-        // sort the numbers to handle duplicates
-        Arrays.sort(nums);
         List<List<Integer>> subsets = new ArrayList<>();
+        // start by adding the empty subset
         subsets.add(new ArrayList<>());
-        int startIndex = 0, endIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            startIndex = 0;
-            // if current and the previous elements are same, create new subsets only from the subsets
-            // added in the previous step
-            if (i > 0 && nums[i] == nums[i - 1])
-                startIndex = endIndex + 1;
-            endIndex = subsets.size() - 1;
-            for (int j = startIndex; j <= endIndex; j++) {
-                // create a new subset from the existing subset and add the current element to it
-                List<Integer> set = new ArrayList<>(subsets.get(j));
-                set.add(nums[i]);
+        for (int currentNumber : nums) {
+            // we will take all existing subsets and insert the current number in them to create new subsets
+            int n = subsets.size();
+            for (int i = 0; i < n; i++) {
+                // create a new subset from the existing subset and insert the current element to it
+                List<Integer> set = new ArrayList<>(subsets.get(i));
+                set.add(currentNumber);
                 subsets.add(set);
             }
         }
@@ -28,10 +22,10 @@ class Solution29 {
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> result = findSubsets(new int[]{1, 3, 3});
+        List<List<Integer>> result = Subsets.findSubsets(new int[]{1, 3});
         System.out.println("Here is the list of subsets: " + result);
 
-        result = findSubsets(new int[]{1, 5, 3, 3});
+        result = Subsets.findSubsets(new int[]{1, 5, 3});
         System.out.println("Here is the list of subsets: " + result);
     }
 }
