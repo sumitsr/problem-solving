7fd994531854602bcc058903e26c3cdf3437b6b1
Sumit Srivastava <sumit.srivastava2805@gmail.com>
2021-07-04 00:39:42 +0100

    feat: Check If Kth Bit Is Set/Unset

---
+++ src/main/java/com/problemsolving/bitmanipulation/Solution14_2.java
@@ -0,0 +1,25 @@
+package com.problemsolving.bitmanipulation;
+
+class Solution14_2 {
+    private static int helper(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int k = 1;
+        
+        while (true) {
+            if (((n >> (k - 1)) & 1) == 0) {
+                k++;
+            } else {
+                return k;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("First setbit position for number: 18 is -> " + helper(18));
+        System.out.println("First setbit position for number: 5 is -> " + helper(5));
+        System.out.println("First setbit position for number: 32 is -> " + helper(32));
+    }
+}
