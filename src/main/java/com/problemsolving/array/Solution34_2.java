bfcdadae0c0af53a8703b4ab76d66c066696cf5a
Sumit Srivastava <sumit.srivastava2805@gmail.com>
2021-07-11 13:08:01 +0100

    feat: Pair with Target Sum

---
+++ src/main/java/com/problemsolving/array/Solution34_2.java
@@ -0,0 +1,24 @@
+package com.problemsolving.array;
+
+import java.util.HashMap;
+
+class Solution34_2 {
+
+  public static int[] search(int[] arr, int targetSum) {
+    HashMap<Integer, Integer> nums = new HashMap<>(); // to store numbers and their indices
+    for (int i = 0; i < arr.length; i++) {
+      if (nums.containsKey(targetSum - arr[i]))
+        return new int[] { nums.get(targetSum - arr[i]), i };
+      else
+        nums.put(arr[i], i); // put the number and its index in the map
+    }
+    return new int[] { -1, -1 }; // pair not found
+  }
+
+  public static void main(String[] args) {
+    int[] result = search(new int[] { 1, 2, 3, 4, 6 }, 6);
+    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+    result = search(new int[] { 2, 5, 9, 11 }, 11);
+    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+  }
+}
